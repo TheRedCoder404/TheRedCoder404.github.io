95c4beee7701ce95a8e8a4ceb0481a0c90671e78
TheRedCoder404 <jm.jasper05@gmx.de>
2025-05-15 16:33:06 +0200

    Automatisches Alter im Haupttext
    Habe ein js ordner und js script hinzugefügt, um mein alter im Haupttext automatisch ändern zu lassen.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function HasBeenYears(dt1)
+{
+    dt2 = new Date()
+
+    var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+    diff /= (60 * 60 * 24);
+    return Math.abs(Math.floor(diff/365.25));
+}
